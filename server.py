34648928a2597f6a9a80ed0553aad4f6e24e44a9
Sathariz <92373527+Sathariz@users.noreply.github.com>
2022-03-06 20:36:11 +0100

    Added mime-type, tests and necessary files

---
+++ server.py
@@ -0,0 +1,75 @@
+import socketserver, os
+from pathlib import Path
+
+from http_serv.http_exceptions import Http404Exception
+from http_serv.http_status import HttpStatusCode
+from http_serv.utils import (
+    parse_first_line,
+    parse_headers,
+    build_response_headers,
+    build_status_line,
+    identify_resource,
+    read_resource,
+)
+
+
+class HttpServer(socketserver.BaseRequestHandler):
+    def handle(self):
+        try:
+            print("Handling request...")
+
+            data = self.request.recv(4096)
+            print("Receive data")
+            request: str = data.decode()
+            print("Request:")
+            print(request)
+
+            try:
+                first_line_str, req_headers_str = request.split("\r\n", maxsplit=1)
+                req_headers_str = req_headers_str.replace("\r\n", "\n")
+                parsed_first_line = parse_first_line(first_line_str)
+                req_headers = parse_headers(req_headers_str)
+
+                ####
+
+                resource_path, mime_type = identify_resource(
+                    "http_serv/public_html", parsed_first_line["resource"]
+                )
+                response_body, resource_len = read_resource(resource_path)
+
+                ###
+
+                status_line = build_status_line(HttpStatusCode.OK)
+                response_headers = build_response_headers(resource_len, mime_type)
+
+            except Http404Exception as e:
+                status_line = build_status_line(HttpStatusCode.NOT_FOUND)
+                response_body = (
+                    f"<h1>404 Not Found</h1>\nCannot found resource {e.resource}"
+                )
+                response_headers = build_response_headers(len(response_body))  # ?
+            except Exception as e:
+                status_line = build_status_line(HttpStatusCode.INTERNAL_SERVER_ERROR)
+                response_headers = build_response_headers(0)  # ?
+                response_body = ""
+
+            ###
+
+            response = f"{status_line}\r\n"
+            response += response_headers + "\r\n\r\n"
+            response += response_body
+
+            ###
+
+            self.request.sendall(response.encode())
+        except:
+            pass
+
+
+def main():
+    with socketserver.TCPServer(("localhost", 8091), HttpServer) as server:
+        server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_build_response_headers.py
+++ tests/test_build_response_headers.py
@@ -1,21 +1,56 @@
 from http_serv.server import build_response_headers
 
 
-def test_build_resposne_headers_x():
+def test_build_resposne_headers_x_css():
     resrc_len = "x"
+    mime_typ = "text/css"
 
-    expected = "Keep-alive: Close\r\nServer: http_serv\r\nContent-Length: x"
+    expected = "Keep-alive: Close\r\nServer: http_serv\r\nContent-Length: x\r\nContent-type: text/css"
 
-    actual = build_response_headers(resrc_len)
+    actual = build_response_headers(resrc_len, mime_typ)
 
     assert expected == actual
 
 
-def test_build_resposne_headers_complex():
+def test_build_resposne_headers_complex_css():
     resrc_len = "Mn+34Df6/78"
+    mime_typ = "text/css"
 
-    expected = "Keep-alive: Close\r\nServer: http_serv\r\nContent-Length: Mn+34Df6/78"
+    expected = "Keep-alive: Close\r\nServer: http_serv\r\nContent-Length: Mn+34Df6/78\r\nContent-type: text/css"
 
-    actual = build_response_headers(resrc_len)
+    actual = build_response_headers(resrc_len, mime_typ)
+
+    assert expected == actual
+
+
+def test_build_resposne_headers_complex_html():
+    resrc_len = "Mn+34Df6/78"
+    mime_typ = "text/html"
+
+    expected = "Keep-alive: Close\r\nServer: http_serv\r\nContent-Length: Mn+34Df6/78\r\nContent-type: text/html"
+
+    actual = build_response_headers(resrc_len, mime_typ)
+
+    assert expected == actual
+
+
+def test_build_resposne_headers_y_json():
+    resrc_len = "y"
+    mime_typ = "application/json"
+
+    expected = "Keep-alive: Close\r\nServer: http_serv\r\nContent-Length: y\r\nContent-type: application/json"
+
+    actual = build_response_headers(resrc_len, mime_typ)
+
+    assert expected == actual
+
+
+def test_build_resposne_headers_y_other():
+    resrc_len = "y"
+    mime_typ = "application/octet-stream"
+
+    expected = "Keep-alive: Close\r\nServer: http_serv\r\nContent-Length: y\r\nContent-type: application/octet-stream"
+
+    actual = build_response_headers(resrc_len, mime_typ)
 
     assert expected == actual
