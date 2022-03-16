a198f980c25ac080314e0b510682ac060c7b67f1
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2022-03-16 21:05:01 +0100

    basic auth

--- http_serv/http_exceptions.py
+++ http_serv/http_exceptions.py
@@ -2,6 +2,8 @@ class Http404Exception(Exception):
     def __init__(self, resource):
         self.resource = resource
 
+class Http403Exception(Exception):
+    pass
 
 class Http500Exception(Exception):
     pass
